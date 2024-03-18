7ceb88141eeb2deb534f93957c62a28087ded0cf
DevVivekk <vivekbhardwajvk2202@gmail.com>
2024-03-19 02:21:14 +0530

    added infite image scroll

--- src/app/page.js
+++ src/app/page.js
@@ -75,7 +75,7 @@ const handelNavigate = (item)=>{
       <section><ReactTypingEffect eraseSpeed={1} style={{"fontSize":"2rem"}} cursor="/"
         text={["Create your own token", "Share it with the world!"]}
       /></section>
-    <h2 onClick={()=>window.open("/home")} style={{"fontSize":"2rem"}}>Go the Main Page <MdOutlineArrowOutward size={'2.5rem'} color='white' /></h2>
+    <h2 onClick={()=>window.open("/home")} style={{"fontSize":"2rem"}}>Go To the Main Page <MdOutlineArrowOutward size={'2.5rem'} color='white' /></h2>
     </div>
     <div className='meme-list'>
     {
