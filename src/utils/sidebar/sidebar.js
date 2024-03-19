9f4ae66ba22680f8a6d7b58ac29a724f23487f48
DevVivekk <vivekbhardwajvk2202@gmail.com>
2024-03-19 20:01:03 +0530

    smoothen the sidebar transition

--- src/utils/sidebar/sidebar.js
+++ src/utils/sidebar/sidebar.js
@@ -5,11 +5,11 @@ const Sidebar = ({hide}) => {
   return (
     <div style={hide ? { transform: "translateX(100%)" } : { transform: "translateX(0%)" }} id='sidebar' className='main-sidebar'>
     <ul>
-        <li style={hide ? { transform: "translateX(400%)" } : { transform: "translateX(0%)" }}>About</li>
-        <li style={hide ? { transform: "translateX(400%)" } : { transform: "translateX(0%)" }}>Services</li>
-        <li style={hide ? { transform: "translateX(400%)" } : { transform: "translateX(0%)" }}>Connect</li>
-        <li style={hide ? { transform: "translateX(400%)" } : { transform: "translateX(0%)" }}>Join Us</li>
-        <li style={hide ? { transform: "translateX(400%)" } : { transform: "translateX(0%)" }}>Brown Paper <MdOutlineArrowOutward size={'3rem'} /></li>
+        <li style={hide ? { transform: "translateX(400%)",transition: "all ease 0s 0s" } : { transform: "translateX(0%)" }}>About</li>
+        <li style={hide ? { transform: "translateX(400%)",transition: "all ease 0s 0s" } : { transform: "translateX(0%)" }}>Services</li>
+        <li style={hide ? { transform: "translateX(400%)",transition: "all ease 0s 0s" } : { transform: "translateX(0%)" }}>Connect</li>
+        <li style={hide ? { transform: "translateX(400%)",transition: "all ease 0s 0s" } : { transform: "translateX(0%)" }}>Join Us</li>
+        <li style={hide ? { transform: "translateX(400%)",transition: "all ease 0s 0s" } : { transform: "translateX(0%)" }}>Brown Paper <MdOutlineArrowOutward size={'3rem'} /></li>
     </ul>
     </div>
   )
